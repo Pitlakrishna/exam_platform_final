b116e3ebbdf3ae25d549a8fbd30aa60bfbe6f247
Shashank <shashankmende88@gmail.com>
2024-08-13 14:31:05 +0530

    Added profile picture feature

--- src/components/NavProfile/index.js
+++ src/components/NavProfile/index.js
@@ -6,8 +6,17 @@ import { useNavigate } from "react-router-dom";
 
 export const NavProfile = () => {
     const navigate = useNavigate()
+
+    const onClickLogout =()=>{
+        const storedEmail = JSON.parse(localStorage.getItem('email'))
+        localStorage.removeItem("email")
+        localStorage.removeItem(storedEmail)
+        navigate("/")
+    }
+
+
     return (
-        <li className="d-flex align-items-center nav-profile" onClick={() => navigate("/")} >
+        <li className="d-flex align-items-center nav-profile" onClick={() => onClickLogout()} >
             {/* <IoPerson /> */}
             <IoMdExit />
         </li>
